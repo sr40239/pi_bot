d406dc4f569968b932d71322af989f017bd2123f
sr40239 <sr40239@gmail.com>
2018-12-16 22:06:09 +0900

    ぴのbotプログラム追加

---
+++ pi.py
@@ -0,0 +1,29 @@
+import discord
+TOKEN = "NTIzMTQ1NDIzNjUzOTYxNzI5.DvVTbw.fj-spnbj_TpWhiSqQtzOCC5wrjw"
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("Logged in " + client.user.name)
+    print("-----")
+
+@client.event
+async def on_message(message):
+    if client.user != message.author:
+        repMsg = recognizeWord(message)
+        if repMsg != "":
+            await client.send_message(message.channel, repMsg)
+
+
+# ぴが返してくれる言葉
+def recognizeWord(message):
+    # 今はまだおうむ返し
+    words = {}
+    words['すき'] = "私もです!" + message.author.name + "さん!"
+    words['かわいい'] = "ありがとうございます えへへ(//∇//)"
+
+    word = words.get(message.content, "")
+    print("word = " + word)
+    return word
+
+client.run(TOKEN)
